179b42a86bb2292b0b8b6f0b70106c008c06d9e7
vitoriakaroline2091-cmd <vitoriakaroline2091@gmail.com>
2025-11-12 16:06:03 -0300

    adicionando um novo arquivo chamado aluno.js,basicamente ver a nota de quem ta aprovado e quem ta reprovado

---
+++ logica-programacall/aluno.js
@@ -0,0 +1,12 @@
+let nota = Number(prompt("Digite o numero da nota"));
+if(nota >6){
+    alert("O numero é maior que seis");
+    
+
+}else if (nota <6); {
+    alert ("O numero é menor que seis.");
+    
+
+} 
+
+
--- logica-programacall/numeros.js
+++ logica-programacall/numeros.js
@@ -6,5 +6,13 @@ positivo,negativo ou igual a zero*/
 let numero = Number(prompt("Quero um numero"));
 
 if (numero >0) {
+console.log("positivo")
+}else if (numero ===0) {
+    console.log("igual a zero"); 
+    
+    }else {
+        console.log("negativo")
+    }
 
-}
+
+
