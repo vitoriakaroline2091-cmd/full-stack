23046a79867ef3f85f6f6a8dfaf3e37327d5273e
vitoriakaroline2091-cmd <vitoriakaroline2091@gmail.com>
2025-11-12 16:08:19 -0300

    editando o arquivo do aluno.js

--- logica-programacall/aluno.js
+++ logica-programacall/aluno.js
@@ -1,10 +1,10 @@
 let nota = Number(prompt("Digite o numero da nota"));
 if(nota >6){
-    alert("O numero é maior que seis");
+    alert("Aprovado");
     
 
 }else if (nota <6); {
-    alert ("O numero é menor que seis.");
+    alert ("Reprovado");
     
 
 } 
