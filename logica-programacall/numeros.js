f7eb75cf70b6661f67c35220b7409c2f78b92247
vitoriakaroline2091-cmd <vitoriakaroline2091@gmail.com>
2025-11-12 15:39:35 -0300

    adicionei um novo arquivo chamado numero.js

---
+++ logica-programacall/numeros.js
@@ -0,0 +1,10 @@
+/*
+Enunciado:
+Crie um programa que peça um  numero e mostre uma mensagem dizendo se ele é 
+positivo,negativo ou igual a zero*/
+
+let numero = Number(prompt("Quero um numero"));
+
+if (numero >0) {
+
+}
