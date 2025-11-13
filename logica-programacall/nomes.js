e1546abf840c0c7ae570ace4a0bdbde6f5355ee1
vitoriakaroline2091-cmd <vitoriakaroline2091@gmail.com>
2025-11-13 16:12:49 -0300

    adicionando um novo arquivo chamado numeros.js e loop,js

---
+++ logica-programacall/nomes.js
@@ -0,0 +1,8 @@
+const names = ["B2","Rodrigo","Victoria","Dudu", "Alemão", "Doris"];
+//              0     1           2         3       4          5
+
+for(let i = 0; i < names.length; i++) {
+    console.log("-" + names[i])
+}
+
+
---
+++ logica-programacall/numeros-for.js
@@ -0,0 +1,5 @@
+const number = ["0","1","2","3","4","5"];
+//
+for(let i = 0; i < 6 ; i++) {
+    console.log(`Esse número é ${i}`);
+}
