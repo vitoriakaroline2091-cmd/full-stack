74e5583df5295eef4eeede82244b93fd5cd690f5
vitoriakaroline2091-cmd <vitoriakaroline2091@gmail.com>
2025-11-13 15:01:10 -0300

    adicionando um novo arquivo chamado pontos.js

---
+++ logica-programacall/pontos.js
@@ -0,0 +1,17 @@
+/*
+Vamos criar um programa de pontos ganhos do seu time de jogos 
+Pesando no programa:
+*/
+const vitorias = Number(promp("Quantos jogos o seu time venceu"));
+const empates = Number(prompt("Quantos jogos seu time empatou"));
+const pontos = (vitorias * 3 + empates);
+function mostra(mensagem) {
+    document.write(mensagem + pontos);
+}
+if(pontos>10) {
+  mostra("Seu time foi muito bem");
+}else if (pontos < 5) {
+    mostra("Seu time precisa melhorar");
+}else {
+    mostra("Fora do rebaixamento");
+}
