559da2f03858fb80afa9cf7b2bf9f833fa2b8804
vitoriakaroline2091-cmd <vitoriakaroline2091@gmail.com>
2025-11-17 16:29:04 -0300

    adicionando novas informações

---
+++ smart-contatos/js/script.js
@@ -0,0 +1,18 @@
+const inputNome = document.getElementById("nome");
+const inputEmai = document.getElementById("email");
+
+//inputEmail
+//inputTel
+
+function btnCadastrar(event) {
+   event.preventDefault();
+   console.log(event);
+    console.log(inputNome.value);
+    console.log(inputEmai.value);
+    console.log(inputTel.value);
+}
+
+
+
+
+
