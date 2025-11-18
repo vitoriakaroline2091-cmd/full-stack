cb764c0af4d8129e6a67aa21758c65631f6b9c28
vitoriakaroline2091-cmd <vitoriakaroline2091@gmail.com>
2025-11-18 15:02:18 -0300

    adicionando informações

--- smart-contatos/js/script.js
+++ smart-contatos/js/script.js
@@ -1,16 +1,33 @@
-const inputNome = document.getElementById("nome");
+const form = document.querySelector("form");
+ const inputNome = document.getElementById("nome");
 const inputEmai = document.getElementById("email");
 const inputTel = document.getElementById("telefone");
-//inputEmail
-//inputTel
 
-function btnCadastrar(event) {
-   event.preventDefault();
-   console.log(event);
-    console.log(inputNome.value);
-    console.log(inputEmai.value);
-    console.log(inputTel.value);
-}
+ form = document.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    //Validação do formulário
+    /*
+    se o input for vazio, adicionando uma mensagem
+    */
+   if(inputNome.value=="") {
+    alert("Digite o nome");
+    return false;
+   }
+   if(inputEmai.value=="") 
+    alert("Digite o email");
+
+
+
+    console.log("Nome",inputNome.value);
+    console.log("Email",inputEmai.value);
+    console.log("Telefone",inputTel.value);
+    
+  })
+
+
+
+
 
 
 
