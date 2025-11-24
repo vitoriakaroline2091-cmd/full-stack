f52f3afea0919c4d3e3c46d9494fd2dec29df966
vitoriakaroline2091-cmd <vitoriakaroline2091@gmail.com>
2025-11-24 16:22:37 -0300

    adicionando novos arquivos

--- node-aula01/app.js
+++ node-aula01/app.js
@@ -1,8 +1,14 @@
 const express = require("express");
-const app = express ();
+const app = express();
 const port = 3000;
 
-//Variável com d
+//Variável com os dados produtos
+const produtos = [
+    { id: 1, nome: 'Produto A', preco: 100 },
+    { id: 2, nome: 'Produto B', preco: 150 },
+    { id: 3, nome: 'Produto C', preco: 200 },
+]
+
 
 // Criar função
 //Rota principal
@@ -11,6 +17,16 @@ app.get('/', (req, res) => {
 
 })
 
+//Rota principal
+app.get('/produtos', (req, res) => {
+    res.send(produtos);
+})
+
+// Rota home
+app.get('/home', (req, res) => {
+    // console.log("olá:",__dirname);
+    res.sendFile(__dirname + '/public/home.html');
+})
 // Executando o servidor
 app.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`);
