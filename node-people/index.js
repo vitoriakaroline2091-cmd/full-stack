59e1978ded64b20f6eb2ca139d536e2c97960011
vitoriakaroline2091-cmd <vitoriakaroline2091@gmail.com>
2025-11-26 15:41:11 -0300

    adicionando uma nova pasta e uma nova porta

---
+++ node-people/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('public'));
+
+// mock
+const nomes = [
+    { id: 1, nome: "Fernanda", idade: "18" },
+    { id: 2, nome: "Caio", idade: "23" },
+    { id: 3, nome: "Pedro", idade: "56" },
+    { id: 4, nome: "Samuel", idade: "45" },
+    { id: 5, nome: "Doris", idade: "33" },
+];
+
+
+
+app.get('/teste', (req, res) => {
+    res.send("API nodePeople está funcionando! okkkkkk");
+});
+
+app.get('/listaNomes', (req, res) => {
+    res.send(nomes);
+});
+
+// Buscando nomes (ListaNomes)
+
+
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando no endereço http://localhost:${PORT}`);
+});
