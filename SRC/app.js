3300dd71849c61fd676f534e269e46003acb5d65
vitoriakaroline2091-cmd <vitoriakaroline2091@gmail.com>
2025-12-08 15:32:05 -0300

    adicionando mais dois times na selecao

--- SRC/app.js
+++ SRC/app.js
@@ -6,8 +6,16 @@ const app = express();
 
 
 app.get("/", (req, res) => {
-    res.send("Olá Node js")
+    res.send("Olá Copa do Mundo!");
 });
 
+//Buscar todas as seleções 
+app.get ('/selecoes', (req, res)=> {
+    const sql = "select * from selecoes";
+
+    conexao.query(sql, (erro, result)=> {
+        res.json(result);
+    });
+});
 
 export default app;
