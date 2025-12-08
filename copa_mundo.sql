2ab38be595f9a8f5708064ab48fe191dbdb5fb7e
vitoriakaroline2091-cmd <vitoriakaroline2091@gmail.com>
2025-12-08 14:30:04 -0300

    adicionando um novo arquivo chamado copa mundo

---
+++ copa_mundo.sql
@@ -0,0 +1,23 @@
+-- Criar banco de dados
+create database copa_mundo;
+
+use copa_mundo;
+
+-- Cria a tabela 'selecoes' com tres colunas
+create table selecoes (
+id int auto_increment primary key, -- ID ÚNICO onde aumenta automaticamente
+selecao varchar(100) not null, -- nome  da selecao, texto ate 100 caracteres
+grupo varchar(1) not null  -- grupo da selecao , apenas uma letra (A,B,C)
+);
+
+select * from selecoes;
+
+-- insere varias selecoes de uma vez na tabela
+insert into selecoes (selecao, grupo) values
+  ('Brasil', 'A'),
+  ('Franca', 'B'),
+  ('Ilhas Salomao', 'C'),
+  ('Nigeria', 'D'),
+  ('Nova Zelandia', 'E'),
+  ('Japao', 'F');
+ 
