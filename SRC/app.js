e5d916ac8f971d90b2204a33a74d7c381f8f25dc
vitoriakaroline2091-cmd <vitoriakaroline2091@gmail.com>
2025-12-08 16:17:00 -0300

    fazendo  outra porta

--- SRC/app.js
+++ SRC/app.js
@@ -18,4 +18,27 @@ app.get ('/selecoes', (req, res)=> {
     });
 });
 
+// buscando por ID
+app.get('/selecoes/:id', (req, res)=> {
+    const id = req.params.id;
+    const sql = "select * from selecoes where id=?;";
+
+    conexao.query(sql, id ,(erro,result)=> {
+        res.json(result[0]);
+    });
+
+});
+
+// Deletando registro
+app.delete('/selecoes/:id', (req, res)=> {
+    const id = req.params.id;
+   const sql = "delete from selecoes where id=?";
+
+    conexao.query(sql, id, ()=> {
+        res.json({mensagem: "Deletando com sucesso"});
+    });
+})
+
+
+
 export default app;
