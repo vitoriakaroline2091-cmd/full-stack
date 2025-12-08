a91fea82a6e692b574b10d28fb6795ebbfd4c7fb
vitoriakaroline2091-cmd <vitoriakaroline2091@gmail.com>
2025-12-08 14:17:00 -0300

    espaco

--- script.sql
+++ script.sql
@@ -17,6 +17,8 @@ insert into produtos (nome, email, telefone, cpf, endereco) values
 ('Eduardo Ramos', 'edu.ramos@orutos.com', '1234567-777', '111.444.77-00','Rua Orutos F'),
 ('Juliana Clara', 'juliana@orutos.com', '7777628-789','777.333.777-11', 'Rua Orutos');
 
+
+
 -- Seleciona o registro onde o id = 1
 select * from produtos  where id = 2;
 
